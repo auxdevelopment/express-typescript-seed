9b9dd1b1b304b7c24bbff30d49fc3d418380b65c
Benjamin Auinger <benjamin.au.99@gmx.at>
2019-03-14 22:14:54 +0100

    add server file

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const bodyparser = require('body-parser');
+const cors = require('cors');
+
+const SERVER_PORT = 3000;
+const routes = require('./routes/routes');
+
+
+// ----------------- middleware configuration -------------------------
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+app.use(cors());
+// ----------------- end middlerware config   -------------------------
+
+app.use('/', routes);
+
+app.listen(SERVER_PORT, function () {
+  console.log('Server started on ' + SERVER_PORT);
+});
+
+module.exports = app;
+
